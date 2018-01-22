bd52d149128d5635c6cf4c2677df906daf7b19a7
Michael Kuehl <michael@ratchet.cc>
2018-01-22 10:01:15 +0100

    cluster size examples

--- roles/prepare_inventories/files/install-openshift.sh
+++ roles/prepare_inventories/files/install-openshift.sh
@@ -5,15 +5,8 @@
 #
 # ./install-openshift.sh |& tee install-openshift.log
 #
-# If no inventory is provided, the default single-master installation is started.
 # 
 
-INVENTORY=$1
-if [ "$INVENTORY" == "" ]; then
-  INVENTORY="single_master_inventory.cfg"
-fi
-
 export ANSIBLE_HOST_KEY_CHECKING=False
-export INVENTORY
 
-ansible-playbook -i ~{{instance_user}}/$INVENTORY ~{{instance_user}}/openshift-ansible/playbooks/byo/config.yml
+ansible-playbook -i ~{{instance_user}}/inventory.cfg ~{{instance_user}}/openshift-ansible/playbooks/byo/config.yml
