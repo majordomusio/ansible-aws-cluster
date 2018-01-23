56903bdb5d143cc5280cf80e9778216aaf9afe38
Michael Kuehl <michael@ratchet.cc>
2018-01-23 09:44:20 +0100

    cosmetics

--- roles/prepare_inventories/files/install-openshift.sh
+++ roles/prepare_inventories/files/install-openshift.sh
@@ -9,4 +9,13 @@
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 
+start_time=$(date +%s)
+
 ansible-playbook -i ~{{instance_user}}/inventory.cfg ~{{instance_user}}/openshift-ansible/playbooks/byo/config.yml
+
+end_time=$(date +%s)
+dt=$(echo "$end_time - $start_time" | bc)
+dm=$(echo "$dt/60" | bc)
+ds=$(echo "$dt-60*$dm" | bc)
+
+printf "\nTotal execution time of playbook: %02dm %02ds.\n\n" $dm $ds
