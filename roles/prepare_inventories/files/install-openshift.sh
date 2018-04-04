e4ed790ce9c9940c481ea643dc8b0937b34b3d75
Michael Kuehl <michael@ratchet.cc>
2018-04-04 22:13:21 +0200

    adjustments for 3.9

--- roles/prepare_inventories/files/install-openshift.sh
+++ roles/prepare_inventories/files/install-openshift.sh
@@ -9,13 +9,5 @@
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 
-start_time=$(date +%s)
-
-ansible-playbook -i ~{{instance_user}}/inventory.cfg ~{{instance_user}}/openshift-ansible/playbooks/byo/config.yml
-
-end_time=$(date +%s)
-dt=$(echo "$end_time - $start_time" | bc)
-dm=$(echo "$dt/60" | bc)
-ds=$(echo "$dt-60*$dm" | bc)
-
-printf "\nTotal execution time of playbook: %02dm %02ds.\n\n" $dm $ds
+ansible-playbook -i ~{{instance_user}}/inventory ~{{instance_user}}/openshift-ansible/playbooks/prerequisites.yml
+ansible-playbook -i ~{{instance_user}}/inventory ~{{instance_user}}/openshift-ansible/playbooks/deploy_cluster.yml
